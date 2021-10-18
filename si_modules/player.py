586481c9be028b4f8480f75e0514daa522c6b419
Laraib <laraib@pop-os.localdomain>
2021-10-18 16:33:17 +0530

    refactor: player

--- si_modules/player.py
+++ si_modules/player.py
@@ -29,25 +29,17 @@ class Player(Ship):
     def update(self):
         '''Update the player's postion based on movement flag.'''
 
-        # store a decimal value for the player's center.
-        self.centery = float(self.rect.centery)
-        self.centerx = float(self.rect.centerx)
-
         if self.moving_up and self.rect.top > 0:
-            self.centery -= self.si_settings.player_speed_factor
+            self.rect.centery -= self.si_settings.player_speed_factor
 
         if self.moving_down and self.rect.bottom < self.screen_rect.bottom:
-            self.centery += self.si_settings.player_speed_factor
+            self.rect.centery += self.si_settings.player_speed_factor
 
         if self.moving_right and self.rect.right < self.screen_rect.right:
-            self.centerx += self.si_settings.player_speed_factor
+            self.rect.centerx += self.si_settings.player_speed_factor
 
         if self.moving_left and self.rect.left > self.screen_rect.left:
-            self.centerx -= self.si_settings.player_speed_factor
-
-        # update rect objectfrom self.center
-        self.rect.centery = self.centery
-        self.rect.centerx = self.centerx
+            self.rect.centerx -= self.si_settings.player_speed_factor
 
 
     def fire_bullet(self):
